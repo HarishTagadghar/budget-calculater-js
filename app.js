b40ad1377d9a895f86fdb86801c472dce2d707f9
harish tagadghar <harishtgtg@gmail.com>
2020-03-18 22:01:58 +0530

    starting files

---
+++ app.js
@@ -0,0 +1,25 @@
+let budgetcontroller = (function(){
+
+})(); // therse types of functions are called iifi function 
+
+let uicontroller = (function () {
+
+})();
+
+let controller = (function(){
+
+    let cntAddItem = function (){
+        console.log('it works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click',cntAddItem);
+
+   document.addEventListener('keypress',function(e){
+       if ( e.keyCode === 13){
+           cntAddItem()
+       }
+   })
+
+
+
+})();
