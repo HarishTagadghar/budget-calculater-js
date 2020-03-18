c5842837ac0ceafa321d7ef281b6b0dcbe86c82f
harish tagadghar <harishtgtg@gmail.com>
2020-03-18 23:02:00 +0530

    let cntAddItem = function (){

--- app.js
+++ app.js
@@ -4,23 +4,65 @@ let budgetcontroller = (function(){
 
 
 let uicontroller = (function () {
-
-})();
-
-let controller = (function(){
-
-    let cntAddItem = function (){
-        console.log('it works');
+    
+    let domstring = {
+            inputtype:'.add__type',
+            inputdescription:'.add__description',
+            inputvalue:'.add__value',
+            inputbtn:'.add__btn'
     }
 
-    document.querySelector('.add__btn').addEventListener('click',cntAddItem);
 
-   document.addEventListener('keypress',function(e){
-       if ( e.keyCode === 13){
-           cntAddItem()
-       }
-   })
+
+    return {
+        getinput: function (){
+            return {
+                type : document.querySelector(domstring.inputtype).value,
+                description : document.querySelector(domstring.inputdescription).value,
+                value : document.querySelector(domstring.inputvalue).value
+            }
+        },
+        dominput : function(){
+           return domstring ;
+        }
+    }
 
 
 
 })();
+
+let controller = (function(bdtctl,uictl){
+
+    let cntAddItem = function (){
+        console.log(uictl.getinput());
+    }
+
+let seteventlistener = function(){
+    let dom = uictl.dominput();
+
+    document.querySelector(dom.inputbtn).addEventListener('click',cntAddItem);
+
+    document.addEventListener('keypress',function(e){
+        if ( e.keyCode === 13){
+            cntAddItem()
+        }
+    })
+}
+
+   
+return {
+    init : function(){
+        console.log('application is started');
+        seteventlistener();
+        
+    }
+}   
+
+   
+
+
+
+})(budgetcontroller,uicontroller);
+
+
+controller.init();
